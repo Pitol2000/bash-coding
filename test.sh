1aed1e7717d3ff839cc53879526768387d3e7b32
Pitol2000 <joaopedropitol@gmail.com>
2021-12-08 21:50:17 -0500

    made a script

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Hello, this is my setup if you want to install a distro.
+# This contains very basic apps, that you probably want on your pc.
+
+# Uncomment the ones that corresponds to your system.
+
+# sudo apt update && sudo apt upgrade -y
+# sudo pacman -Syyu
+
+# sudo apt-get install neofetch ncdu ranger bpytop cmus libreoffice chromium trash-cli 
+# sudo pacman -S neofetch ncdu ranger bpytop cmus libreoffice chromium trash-cli base-devel
+
+# sudo apt-get update
+# sudo pacman -Syu
+
