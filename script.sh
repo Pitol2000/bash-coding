a9972b7f0432e4c31dbc81326886b572af2ce7bc
Pitol2000 <joaopedropitol@gmail.com>
2021-12-13 21:58:21 -0300

    added tmux

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 echo "Hello, this is my setup if you want to install a distro."
 echo "This contains very basic apps, that you probably want on your pc."
-
+echo ""
 echo "If your distro is debian-based, type 0"
 echo "If your distro is arch-based, type 1"
 
@@ -11,11 +11,11 @@ read  input
 if [ $input == 0 ]
 then
     sudo apt update && sudo apt upgrade -y
-    sudo apt-get install neofetch ncdu ranger bpytop cmus libreoffice chromium trash-cli timeshift vim
+    sudo apt-get install neofetch tmux ncdu ranger bpytop cmus libreoffice chromium trash-cli timeshift vim
     sudo apt-get update
 elif [ $input == 1 ]
 then
     sudo pacman -Syyu
-    sudo pacman -S neofetch ncdu ranger bpytop cmus libreoffice chromium trash-cli base-devel lsd timeshift vim
+    sudo pacman -S neofetch tmux ncdu ranger bpytop cmus libreoffice chromium trash-cli base-devel lsd timeshift vim
     sudo pacman -Syu
 fi
